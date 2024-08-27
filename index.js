c254ff843ad4d69dc654f173167a70f0b737aff7
白宇东 <summer88123@163.com>
2024-08-27 13:59:00 +0800

    适配鸿蒙

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('./ImageCapInset');
+const ReactNative = require('react-native');
+const { Platform } = ReactNative;
+
+module.exports = Platform.OS === 'ios' || Platform.OS === 'harmony' ? require('./ImageCapInset.ios.js') : require('./ImageCapInset.android.js');
